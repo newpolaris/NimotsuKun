f23547a08f0108c9d1c764d3d22dc8679d33ff0c
newpolaris <newpolaris@gmail.com>
2013-10-27 22:07:00 +0900

    Simplify menu input

--- NimotsuKun2D/Sequence/Menu.cpp
+++ NimotsuKun2D/Sequence/Menu.cpp
@@ -5,21 +5,38 @@
 
 void Menu::input()
 {
-	auto f = GameLib::Framework::instance();
+	// 1:やりなおし(다시 고쳐 하기)
+	// 2:面セレへ
+	// 3:タイトルへ
+	// 4:そのまま戻る
+	// 添え字がずれると
+	// 添え(そえ)字がずれると面倒(めんどう)なので0も入れておく
+    // 첨자가 어긋나면 성가시므로 0도 넣어 둔다
+    char numberChars[] = { '0', '1', '2', '3', '4' };
+    int inputNumber = 0;
+    for ( int i = 0; i < 5; ++i ) {
+        if ( GameLib::Framework::instance().isKeyTriggered( numberChars[i] ) ) {
+            inputNumber = i;
+        }
+    }
 
-	if (f.isKeyTriggered('2'))
-		gameInstance().requestSequence(SEQUENCE_STAGE_SELECT);
-    else if (f.isKeyTriggered('3'))
-		gameInstance().requestSequence(SEQUENCE_TITLE);
-    else if (f.isKeyTriggered('4'))
-		gameInstance().requestSequence(SEQUENCE_GAME);
-    else if (f.isKeyTriggered('q'))
-		f.requestEnd();
-    else if (f.isKeyTriggered('1'))
-	{
-		gameInstance().reinit();
-		gameInstance().requestSequence(SEQUENCE_GAME);
-	}
+    switch ( inputNumber ) {
+        case 1: // やりなおし
+            gameInstance().reinit();
+            gameInstance().requestSequence(SEQUENCE_GAME);
+            break;
+        case 2: // 面セレへ
+            gameInstance().requestSequence(SEQUENCE_STAGE_SELECT);
+			break;
+		case 3: // タイトルへ
+            gameInstance().requestSequence(SEQUENCE_TITLE);
+			break;
+		case 4: // そのまま戻る
+            gameInstance().requestSequence(SEQUENCE_GAME);
+			break;
+		default: // その他(ほか)は無視(むし)
+			break;
+    }
 }
 
 void Menu::draw()
